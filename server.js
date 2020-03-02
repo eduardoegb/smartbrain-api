e91ebeb443fecb0ead9ca81a3ee192f5decec385
Eduardo Gonzalez Briceño <eduardoegb@gmail.com>
2020-03-02 10:54:57 -0300

    mend

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const app = express();
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: '17596219',
-    database: 'smartbrain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
